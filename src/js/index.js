0d3f02286b0ba87ad7bfe9b3971495f774397e8c
Sayko003 <konstantin.web@bk.ru>
2025-04-02 09:36:41 +0300

    add sidebar

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,9 @@
 import sliders from "./modules/sliders";
 import aboutAccardion from "./modules/accardion";
-
+import sidebar from "./modules/sidebar";
 document.addEventListener("DOMContentLoaded", () =>{
 
     sliders();
     aboutAccardion();
+    sidebar();
 });
---
+++ src/js/modules/sidebar.js
@@ -0,0 +1,29 @@
+const sidebar = function (){
+    const btn = document.querySelector(`.header--menu`),
+        sidebar = document.querySelector(`.sidebar`),
+        closeHeader = document.querySelector(`.header--menu`),
+        body = document.querySelector(`body`),
+        lg = document.querySelector(`.sidebar--lg`),
+        lgChoise = document.querySelector(`.sidebar--lg-choice`),
+        theme = document.querySelector(`.sidebar--theme`),
+        themeChoise = document.querySelector(`.sidebar--theme-choice`);
+
+
+
+    btn.addEventListener(`click`, (e) =>{
+        sidebar.classList.toggle(`sidebar-active`);
+        closeHeader.classList.toggle(`header--menu-active`);
+    });
+    
+    lg.addEventListener(`click`, (e) =>{
+        e.preventDefault();
+        lgChoise.classList.toggle(`sidebar--lg-choice-active`);
+    })
+
+    theme.addEventListener(`click`, (e) =>{
+        e.preventDefault();
+        themeChoise.classList.toggle(`sidebar--theme-choice-active`);
+    })
+}
+
+export default sidebar;
